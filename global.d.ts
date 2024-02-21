dd1ec681318ed9a9d2a39ae82620b0fc2e26acc8
ivanrussui <fenomen_ny@mail.ru>
2024-02-21 12:23:16 +0300

    config settings

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+// настройки чтобы модульно работали данные импорты
+
+// declare module '*.module.css';
+// declare module '*.svg';
+// declare module '*.jpg';
+// declare module '*.png';
+
+// нужны если удалить файл react-app-env.d
