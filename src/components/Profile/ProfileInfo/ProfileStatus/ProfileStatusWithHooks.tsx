e31a50087568d75593793f6e9032771fee9f7903
Ivan <fenomen_ny@mail.ru>
2025-06-02 14:27:39 +0300

    hooks

---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.tsx
@@ -0,0 +1,40 @@
+import React, {ChangeEvent, FC, FocusEvent, useEffect, useState} from 'react';
+
+type ProfileStatusType = {
+    status: string
+    updateStatusTC: (status: string) => void
+}
+
+export const ProfileStatusWithHooks: FC<ProfileStatusType> = (props) => {
+    const [status, setStatus] = useState<string>(props.status);
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
+    const changeActiveMode = () => {
+        // оба варианта рабочие
+        // setEditMode(!editMode);
+        setEditMode(prevState => !prevState);
+    };
+
+    const onBlurHandler = (e: FocusEvent<HTMLInputElement>) => {
+        changeActiveMode();
+        props.updateStatusTC(e.currentTarget.value);
+    };
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value);
+    };
+
+    return (
+        <div>
+            {editMode
+                ? <input autoFocus onBlur={onBlurHandler} onChange={onChangeHandler}
+                         value={status} type="text"/>
+                : <span onDoubleClick={changeActiveMode}>{props.status || 'No Status'}</span>
+            }
+        </div>
+    );
+};
